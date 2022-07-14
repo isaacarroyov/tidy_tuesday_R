ae1340af1d59bcf194e0f2d527c619149ea785e2
Isaac Arroyo <isaacarroyov@outlook.com>
2022-07-14 04:42:01 -0500

    [UPDATE] Week 28 - European flights - Script - Changed the title

--- gallery_2022/2022_week-28_european-flights.R
+++ gallery_2022/2022_week-28_european-flights.R
@@ -78,7 +78,7 @@ df %>%
   guides(color="none", size = "none") +
   facet_wrap(~country, nrow = 6) +
   coord_cartesian(clip = "off") +
-  labs(title = "European flights and the events caused by COVID-19",
+  labs(title = "European flights and the consequences due to COVID-19",
        subtitle = "<span style='font-size:60px'><b>How do you read the data visualization?</b></span><br>• Each circle represents a month of the year (i.e. June 2019, January 2020 or May 2022), and they are ordered chronologically, starting January 2019 and ending May 2022.<br>• The <b>size</b> of the circle represents the <b>country's total number of flights (arrivals + departures) of the month</b>.<br>• The colour represents whether the number of flights is <span style='color:#DD5540'><b>higher</b></span> than the <b>country's 2019 average</b> or <span style='color:#808349'><b>lower</b></span>. The <b>2019 flights</b> are <span style='color:#924F5A'><b>coloured in the same way</b></span>.<br>• The <b>black line is in March 2020</b>, when most nations started a lockdown.<br><br><span style='font-size:60px'><b>Comments concerning what's displayed</b></span>.<br>The visualization shows the evolution of the number of flights (arrivals + departures) over time. It's clear when the first fall of flights is and how these countries have been struggling recently. Half of them (**France**, **Germany** and the **UK**) **have not yet surpassed their respective country's 2019 average number of flights**. On the other side, the other half have **reached a higher number than their 2019 average**: **Türkiye in July and August 2021, Spain and Italy in May 2021**.",
        caption = "#TidyTuesday Week 28: European Flights<br>Data: Eurocontrol<br>Visualization by Isaac Arroyo (@unisaacarroyov)"
        ) +
@@ -90,7 +90,7 @@ df %>%
     # Facets
     strip.text = element_text(colour = "black", face = "bold", hjust = 0, family = "titleFont", size = 45),
     # Title
-    plot.title = element_textbox_simple(colour = "black", face = "bold", family = "titleFont", lineheight = 0.1, size = 90, margin = margin(5,15,0,5)),
+    plot.title = element_textbox_simple(colour = "black", face = "bold", family = "titleFont", lineheight = 0.1, size = 93, margin = margin(5,15,0,5)),
     plot.title.position = "plot",
     # Subtitle
     plot.subtitle = element_textbox_simple(colour = "black", family = "bodyFont", lineheight = 0.33, size = 40, margin = margin(5,15,20,5)),
