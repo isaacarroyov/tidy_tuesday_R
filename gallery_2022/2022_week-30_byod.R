cc53d19fb6a39f801109b946e1c2268412c07fd7
Isaac Arroyo <isaacarroyov@outlook.com>
2022-07-27 17:19:02 -0500

    [UPDATE] Week 30 - BYOD - Script -> Done with small pic of overall view of wildifres

--- gallery_2022/2022_week-30_byod.R
+++ gallery_2022/2022_week-30_byod.R
@@ -9,13 +9,97 @@ library(ggtext)
 library(sysfonts)
 library(showtextdb)
 library(showtext)
-library(rayshader)
+library(cowplot)
+
+font_add_google("Quattrocento", "titleFont")
+font_add_google("Quattrocento Sans", "bodyFont")
+showtext_auto()
 
 # LOAD DATA -> Points and yucatan region
-firms_data <- geojson_sf("./data/2022/datos_firms_2001_2020.geojson") %>%
-  select(geometry, acq_date)
+firms_data <- geojson_sf("./data/2022/datos_firms_2001_2020.geojson")
 region_yuc <- read_sf("./data/2022/31ent.shp")
 
+# ------------------------------------------------------------------------------
+# DATA PROCESSING 02
+# 1 - Obtener los puntos de las coordenadas
+firms_data_pos <- firms_data %>%
+  as_tibble() %>%
+  select(acq_date, confidence) %>%
+  mutate(acq_date = stringr::str_replace(acq_date, "T00:00:00","")) %>%
+  mutate(acq_date = lubridate::year(acq_date)) %>%
+  bind_cols(firms_data %>%
+              st_coordinates() %>%
+              as_tibble() %>%
+              rename(x_pos = X, y_pos = Y)
+              ) %>%
+  filter(confidence > 84)
+  
+
+# ------------------------------------------------------------------------------
+# DATA VISUALIZATION -> DENSTY PLOT
+
+# DATA-VIS SETTINGS
+theme_set(theme_minimal())
+theme_update(
+  legend.position = "none",
+  panel.grid = element_blank(),
+  axis.title = element_blank(),
+  axis.text = element_blank(),
+)
+
+# Overall view
+overall_view <- firms_data_pos %>%
+  ggplot(aes(x=x_pos, y=y_pos)) +
+  stat_density_2d_filled(n=200, bins = 50, contour = T, size = 0) +
+  scale_fill_manual(values = c("transparent", MetBrewer::met.brewer("OKeeffe2", n=49, direction = 1))) +
+  labs(title = "Over all view: from 2001 to 2020") +
+  theme(
+    # Background
+    plot.background = element_rect(fill = 'transparent', color = "transparent"),
+    panel.background = element_rect(fill = "transparent", colour = "transparent"),
+    plot.title.position = "plot",
+    plot.title = element_text(face = "bold", family = "titleFont", size = 50)
+    )
+
+# Save overall view
+ggsave(filename = "./gallery_2022/2022_week-30_byod_overall_view.png",
+       plot = overall_view, width = 3.5, height = 3, units = "in")
+
+
+# DONE SMALL PIC
+# ------------------------------------------------------------------------------------------
+
+
+p2 <- firms_data_pos %>%
+  ggplot(aes(x=x_pos, y=y_pos)) +
+  stat_density_2d_filled(n=200, bins = 50, contour = T, size = 0) +
+  #scale_fill_manual(values = c("white", NatParksPalettes::natparks.pals("Arches2", 49, direction = -1)))
+  scale_fill_manual(values = c("#FEF8F0", MetBrewer::met.brewer("OKeeffe2", n=49, direction = 1))) +
+  facet_wrap(~acq_date) +
+  labs(title = "20 years of historical hotspots in Yucatan, Mexico",
+       subtitle = "Thanks to the data provided by NASA's Fire Information for Resource Management System (FIRMS), we can see what had been the potential wildfires (confidence higher than 84%) density within Yucatan's territory between 2001 and 2020.<br>The overall view of these potential wildfires suggests a higher density in the southwest region; however, looking at individual years, the visualization showcases that in 2003, there was a much higher concentration of wildfires in the northern part of the state.",
+       caption = "Visualization by Isaac Arroyo<br>Data: NASA's Fire Information for Resource Management System (FIRMS)") +
+  theme(
+    # Background
+    plot.background = element_rect(fill = '#FEF8F0', color = "#FEF8F0"),
+    panel.background = element_rect(fill = "transparent", colour = "transparent"),
+    # Title
+    plot.title.position = "plot",
+    # Subtitle
+    plot.subtitle = element_markdown(),
+    # Caption
+    plot.caption.position = "plot",
+    plot.caption = element_markdown(),
+  )
+
+
+ggsave(filename = "./gallery_2022/2022_week-30_byod.png",
+       plot = p2 ,
+       width = 8.5, height =11, units = "in" )
+
+
+
+# Visualization no elegida
 # ------------------------------------------------------------------------------
 # # DATA PROCESSING 01 (from https://stackoverflow.com/questions/68643517/smoothed-density-maps-for-points-in-using-sf-within-a-given-boundary-in-r)
 # # 1- Cambiar a proyeccion Mercantor (crs = 3857) ambos mapas
@@ -37,32 +121,6 @@ region_yuc <- read_sf("./data/2022/31ent.shp")
 #   st_as_sf() %>%
 #   st_set_crs(3857)
 
-# ------------------------------------------------------------------------------
-# DATA PROCESSING 02
-# 1 - Obtener los puntos de las coordenadas
-firms_data_pos <- firms_data %>%
-  as_tibble() %>%
-  select(acq_date) %>%
-  mutate(acq_date = stringr::str_replace(acq_date, "T00:00:00","")) %>%
-  mutate(acq_date = lubridate::year(acq_date)) %>%
-  bind_cols(firms_data %>%
-              st_coordinates() %>%
-              as_tibble() %>%
-              rename(x_pos = X, y_pos = Y)
-              )
-  
-
-# ------------------------------------------------------------------------------
-# DATA VISUALIZATION -> DENSTY PLOT
-
-# DATA-VIS SETTINGS
-theme_set(theme_minimal())
-theme_update(
-  legend.position = "none",
-  panel.grid = element_blank(),
-  axis.title = element_blank(),
-  axis.text = element_blank(),
-)
 
 # ··················································································
 # DATA VISUALIZATION (Forma 1)
@@ -71,42 +129,6 @@ theme_update(
 #   MetBrewer::scale_fill_met_c("Derain", direction = -1) +
 #   geom_sf(data = st_boundary(region_yuc), size = 0.3, color = 'black') +
 #   coord_sf(ylim = c(22.3e5, 24.6e5), xlim = c(-100.5e5, -97.5e5))
-
-# ··················································································
-# DATA VISUALIZATION (Forma 2)
-firms_data_pos %>%
-  ggplot(aes(x=x_pos, y=y_pos)) +
-  stat_density_2d_filled(n=200, bins = 50, contour = T, size = 0) +
-  #scale_fill_manual(values = c("white", NatParksPalettes::natparks.pals("Arches2", 49, direction = -1)))
-  scale_fill_manual(values = c("#FEF8F0", MetBrewer::met.brewer("OKeeffe2", n=49, direction = 1))) +
-  labs(title = "Hotspot accumulation in Yucatan from 2001 to 2020",
-       subtitle = "",
-       caption = "Visualization by Isaac Arroyo<br>Data: NASA's Fire Information for Resource Management System (FIRMS)") +
-  theme(
-    # Background
-    plot.background = element_rect(fill = '#FEF8F0', color = "#FEF8F0"),
-    panel.background = element_rect(fill = "transparent", colour = "transparent"),
-    # Title
-    plot.title.position = "plot",
-    # Subtitle
-    plot.subtitle = element_markdown(),
-    # Caption
-    plot.caption.position = "plot",
-    plot.caption = element_markdown(),
-  )
-
-
-# ··················································································
-# TODO -> Enhance other day 
-# firms_data_pos %>%
-#   ggplot(aes(x=x_pos,y=y_pos)) + 
-#   stat_density_2d(geom = "raster",
-#                   aes(fill = after_stat(density)),
-#                   contour = F) +
-#   scale_fill_met_c("Derain", direction = -1)
-
-
-
 
 
 
