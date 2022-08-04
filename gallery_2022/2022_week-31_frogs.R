af2a879f6c44ed116d6604f9470e89d52e8b9afe
Isaac Arroyo <isaacarroyov@outlook.com>
2022-08-03 19:51:11 -0500

    [UPDATE] Week 31 - Oregon Spotted Frogs - Script -> Change of plans. Not circle packing -> now dot plot

--- gallery_2022/2022_week-31_frogs.R
+++ gallery_2022/2022_week-31_frogs.R
@@ -2,8 +2,6 @@
 library(dplyr)
 library(ggplot2)
 library(MetBrewer)
-# library(tidygraph)
-library(ggraph)
 library(ggtext)
 library(sysfonts)
 library(showtextdb)
@@ -15,38 +13,18 @@ datos <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidy
 names(datos)
 
 # Select columns for the data visualization
+v_ypos <- c()
+for(x in 16:1){
+  if (x==1) {
+    v_ypos <- append(v_ypos, rep(x, 11))
+  } else {
+    v_ypos <- append(v_ypos, rep(x, 20))
+  }
+}
+
 df <- datos %>%
-  select(Subsite, Water) %>%
-  rename(subsite = Subsite, water = Water) %>%
-  mutate(subsite = stringr::str_replace_all(subsite, " ",""),
-         water = stringr::str_replace_all(water, " ",""),
-         )
-
-
-
-# Create from->to data (edges)
-df_from_to <- df %>%
-  mutate(from = paste(subsite, sep = "."),
-         to = paste(subsite, water, sep = ".")
-         ) %>%
-  select(from, to)
-
-# Create vertices
-df_vertices <- bind_rows(
-  df_from_to %>% count(from) %>% rename(name = from),
-  df_from_to %>% count(to) %>% rename(name = to)
-)
-
-
-
-
-# DATA VISUALIZATION
-gr <- igraph::graph_from_data_frame(df_from_to, vertices = df_vertices)
-gr_02 <- tidygraph::tbl_graph(df_vertices, df_from_to)
-
-set.seed(1)
-ggraph(gr_02, 'circlepack', weight = n) +
-  geom_node_circle(aes(fill=depth)) +
-  geom_node_label(aes(label=name, filter = leaf), size=1.5) +
-  coord_fixed(clip = "off") +
-  theme_minimal()
+  rename(water_type = Type) %>%
+  select(water_type) %>%
+  arrange(water_type) %>%
+  mutate(xpos = c(rep(1:20,15), 1:11),
+         ypos = v_ypos)
