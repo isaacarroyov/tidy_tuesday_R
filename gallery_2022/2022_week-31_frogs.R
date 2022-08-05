7b682de7eede027700d8e0ed56ece12e944d0ec6
Isaac Arroyo <isaacarroyov@outlook.com>
2022-08-05 02:32:19 -0500

    [UPDATE + CREATE] Week 31 - Oregon Spotted Frogs - Crear imagen sin logo para luego incorporarlo en cowplot

--- gallery_2022/2022_week-31_frogs.R
+++ gallery_2022/2022_week-31_frogs.R
@@ -1,19 +1,18 @@
 # TidyTuesday Week 31: Oregon Spotted Frog
 library(dplyr)
 library(ggplot2)
-library(MetBrewer)
 library(ggtext)
 library(sysfonts)
 library(showtextdb)
 library(showtext)
+library(cowplot)
+library(magick)
 
 font_add_google("Montserrat", "bodyFont")
 showtext_auto()
 
 # LOAD DATA
 datos <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-08-02/frogs.csv')
-# Columns
-names(datos)
 
 # Select columns for the data visualization
 v_ypos <- c()
@@ -43,25 +42,29 @@ p1 <- df %>%
   theme_void() +
   coord_fixed(ratio = 1, clip = "off") +
   labs(title = "",
-       subtitle = "Hello hello, everybody, Gossip Girl here. Did you miss me? I know I've missed you. You may have thought that my knowledge only covers information about the scandalous life of Upper East Siders. Nonetheless, you're partially mistaken. In order to deliver the best news is vital to know how to about data, so I decided to visit other places to take a peek at their data. On this occasion, I came up with frogs.<br><br>According to my dear friend, the internet, Mark Twain said, _If it's your job to eat a frog, it's best to do it first thing in the morning. And If it's your job to eat two frogs, it's best to eat the biggest one first_. I don't know about you, but the only time I would put a frog close to my lips, it would be better to become a prince or the heir of a successful company.<br><br>However, the U.S. Geological Survey, a.k.a. USGS, has taken the job of not eating but locating the fantabulous Oregon Spotted Frog from September to November 2018.<br><br>Who would have thought that 175 were on Reservoirs, 93 on Marshes or Ponds, 38 on Streams or Canals and 5 on Non-aquatic places? I don't.<br><br>Remember to keep your eyes open; you don't know what nature will throw at you...XOXO, Gossip Girl.",
+       subtitle = "Hello hello, everybody, Gossip Girl here. Did you miss me? I know I've missed you. You may have thought that my knowledge only covers information about the scandalous life of Upper East Siders. Nonetheless, you're partially mistaken. In order to deliver the best news is vital to know how to about data, so I decided to visit other places to take a peek at their data. On this occasion, I came up with frogs.<br><br>According to my dear friend, the internet, Mark Twain said, _If it's your job to eat a frog, it's best to do it first thing in the morning. And If it's your job to eat two frogs, it's best to eat the biggest one first_. I don't know about you, but the only time I would put a frog close to my lips, it would be better to become a prince or the heir of a successful company.<br><br>However, the U.S. Geological Survey, a.k.a. USGS, has taken the job of not eating but locating the fantabulous Oregon Spotted Frog from September to November 2018.<br><br>Who would have thought that 175 were on Reservoirs, 93 on Marshes or Ponds, 38 on Streams or Canals and 5 on Non-aquatic places? I don't.<br><br>Remember to keep your eyes open; you don't know what nature will throw at you<br><br><br>XOXO, Gossip Girl.",
        caption = "Visualization by Isaac Arroyo.<br>#TidyTuesday Week 31: Oregon Spotted Frogs.<br>Data source: U.S. Geological Survey") +  theme(
     # Background
     plot.background = element_rect(fill = "#15161E", colour = "#15161E"),
-    panel.background = element_blank(),
+    panel.background = element_rect(fill = "#15161E", colour = "#15161E"),
     # Colour Text
     text = element_text(colour = "#FCF199", family = "bodyFont"),
     # Legend 
     legend.position = "none",
     # Title and subtitle
     plot.title.position = "plot",
-    plot.subtitle = element_textbox_simple(size = rel(2.8), width = unit(5,"in"), halign = 0.5, lineheight = 0.4, margin = margin(1.5,0,0.5,0, unit = "in")),
+    plot.subtitle = element_textbox_simple(size = rel(2.8), width = unit(6,"in"), halign = 0.5, lineheight = 0.4, margin = margin(1.5,0,0.2,0, unit = "in")),
     # Caption
-    plot.caption = element_textbox_simple(size = rel(2.5), width = unit(5,"in"), halign = 0.5, lineheight = 0.4, margin = margin(0.5,0,0.5,0, unit = "in")),
+    plot.caption = element_textbox_simple(size = rel(2.5), width = unit(6,"in"), halign = 0.5, lineheight = 0.4, margin = margin(0.2,0,0.5,0, unit = "in")),
   )
 
 
-ggsave(filename = "./gallery_2022/2022_week-31_frogs.png",
+ggsave(filename = "./gallery_2022/2022_week-31_frogs_draft_no_logo.png",
        plot = p1,
        width = 8.5, height = 11, units = "in",
-       dpi = 300, scale = 1
-       )
+       dpi = 300, scale = 1)
+
+
+
+
+
