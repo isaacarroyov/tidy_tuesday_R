dc6f3baa2a675fd7b59eb5570d2558929f523e99
Isaac Arroyo <isaacarroyov@outlook.com>
2022-08-15 14:12:47 -0500

    [UPDATE] Week 32 - Ferris Wheels - Script -> colour and size encoding
    Cambie las variables del eje x y y de ride_duration_minutos, houtly capacity -> max_height_view (creada) y radius (creada)

--- gallery_2022/2022_week-32_ferris-wheels.R
+++ gallery_2022/2022_week-32_ferris-wheels.R
@@ -6,10 +6,6 @@ library(ggtext)
 library(sysfonts)
 library(showtextdb)
 library(showtext)
-# install.packages("FactoMineR")
-# install.packages("factoextra")
-library(FactoMineR)
-library(factoextra)
 
 # Cargamos datos 
 # remotes::install_github("emilhvitfeldt/ferriswheels")
@@ -19,12 +15,32 @@ datos <- wheels %>%
   filter(diameter > 0, hourly_capacity > 0, ride_duration_minutes > 0) %>%
   filter(status == "Operating") %>%
   mutate(radius = diameter/2) %>%
-  select(name, country, height, radius,
-         hourly_capacity, ride_duration_minutes)
+  mutate(max_height_view = height + radius) %>%
+  select(name, country, height, radius, max_height_view)
 
 # Data Visualization -> bubble plot + bar charts
 p1 <- datos %>%
-  ggplot(aes(y=ride_duration_minutes, x=hourly_capacity)) +
-  geom_point()
+  ggplot(aes(x=radius, y = max(max_height_view) - max_height_view, colour = country)) +
+  geom_curve(aes(x=radius, xend = radius, y = max(max_height_view), yend = max(max_height_view) - max_height_view),
+             curvature = 0.1, size = 0.3) +
+  geom_point(aes(size = radius)) +
+  scale_size_area(max_size = 10) +
+  labs(title = "Wachen el paisaje, hommies (Check out the new view, hommies)",
+       subtitle = "Description",
+       caption = "Designed by Isaac Arroyo (@unisaacarroyov on twitter)<br>#TidyTuesday Week 32: Ferris Wheels<br>Data Source: Emil Hvitfeldt (@Emil_Hvitfeldt on twitter) via <span style='font-family:mono'>ferriswheels</span>") +
+  theme_void() +
+  theme(
+    legend.position = "none",
+    # Bacground
+    # Title
+    plot.title.position = 'plot',
+    plot.title = element_textbox_simple(face = 'bold'),
+    # Subtitle
+    plot.subtitle = element_textbox_simple(),
+    # CAption
+    plot.caption.position = 'plot',
+    plot.caption = element_textbox_simple(),
+  )
+
 p1
 
