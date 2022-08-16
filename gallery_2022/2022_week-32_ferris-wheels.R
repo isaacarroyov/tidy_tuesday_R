c36c967e574df5204b5ec79cddf47844826bb65c
Isaac Arroyo <isaacarroyov@outlook.com>
2022-08-15 22:17:08 -0500

    [UPDATE] Week 32 - Ferris Wheels - Script + image -> finished colouring

--- gallery_2022/2022_week-32_ferris-wheels.R
+++ gallery_2022/2022_week-32_ferris-wheels.R
@@ -45,7 +45,7 @@ p1 <- datos %>%
   # Info
   geom_textbox(aes(label = label, x = xpos, y = if_else(condition = xpos%%2 == 0, true = max(max_height_view) + 100, false = max(max_height_view) + 250)),
                size = 6, family = 'englishFont', lineheight = 0.3,
-               width = unit(60,"pt"), fill = 'white',
+               width = unit(60,"pt"), fill = '#FFFCFB',
                box.padding = margin(2,2,2,2), box.r = unit(3,'pt'),
                ) +
   # Annotations
@@ -60,6 +60,8 @@ p1 <- datos %>%
                lineheight = 0.4, width = unit(6,'in')
                ) +
   scale_size_area(max_size = 20) +
+  scale_colour_manual(values = NatParksPalettes::natparks.pals(name = 'DeathValley', n = 9)) +
+  scale_fill_manual(values = NatParksPalettes::natparks.pals(name = 'DeathValley', n = 9)) +
   labs(title = "<span style='font-family:spanishFont'>_Wachen el paisaje, homies_</span> <b>(Check out the new view, homies)</b>",
        subtitle = "Ferris wheels are a well-known attraction in many cities around the globe due to the panoramic view the people can enjoy. The visualization shows some of the most famous Ferris wheels currently operating. <br>The circles' radiuses are size encoded (but not at the same scale as the lines); the maximum height the viewer can be is the distance from the beginning of the line (top) to the center of the circle. Finally, the countries are colour encoded. However, all the information is labelled.",
        caption = "Designed by Isaac Arroyo (@unisaacarroyov on twitter)<br>#TidyTuesday Week 32: Ferris Wheels<br>Data Source: Emil Hvitfeldt (@Emil_Hvitfeldt on twitter) via <span style='font-family:mono'>ferriswheels</span>") +
@@ -68,16 +70,16 @@ p1 <- datos %>%
   theme(
     legend.position = "none",
     # Bacground
-    plot.background = element_rect(fill = 'white', colour = 'white'),
-    panel.background = element_rect(fill = 'white', colour = 'white'),
+    plot.background = element_rect(fill = '#FFFCFB', colour = '#FFFAF9'),
+    panel.background = element_rect(fill = '#FFFAF9', colour = '#FFFAF9'),
     # Title
     plot.title.position = 'plot',
-    plot.title = element_textbox_simple(family = 'englishFont', size = 85, margin = margin(t = 10, b = 10, l = 10)),
+    plot.title = element_textbox_simple(family = 'englishFont', size = 85, margin = margin(t = 15, b = 10, l = 10)),
     # Subtitle
     plot.subtitle = element_textbox_simple(family = 'englishFont', size = 40, lineheight = 0.4, margin = margin(b = 15, l = 10)),
     # CAption
     plot.caption.position = 'plot',
-    plot.caption = element_textbox_simple(family = 'englishFont', face = 'bold', size = 30, lineheight = 0.5, margin = margin(b = 10, l = 10)),
+    plot.caption = element_textbox_simple(family = 'englishFont', face = 'bold', size = 30, lineheight = 0.5, margin = margin(b = 15, l = 10)),
   )
 
 ggsave(filename = "./gallery_2022/2022_week-32_ferris-wheels.png",
