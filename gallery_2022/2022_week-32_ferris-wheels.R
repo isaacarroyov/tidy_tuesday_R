c4a48337daece5f5ac8ab4fbb1e2cb1351102450
Isaac Arroyo <isaacarroyov@outlook.com>
2022-08-15 20:09:19 -0500

    [UPDATE] Week 32 - Ferris Wheels - Script -> added text

--- gallery_2022/2022_week-32_ferris-wheels.R
+++ gallery_2022/2022_week-32_ferris-wheels.R
@@ -1,7 +1,6 @@
 # #TidyTuesday Week 32: Ferris Wheels 
 library(dplyr)
 library(ggplot2)
-library(MetBrewer)
 library(ggtext)
 library(sysfonts)
 library(showtextdb)
@@ -23,10 +22,16 @@ p1 <- datos %>%
   ggplot(aes(x=radius, y = max(max_height_view) - max_height_view, colour = country)) +
   geom_curve(aes(x=radius, xend = radius, y = max(max_height_view), yend = max(max_height_view) - max_height_view),
              curvature = 0.1, size = 0.3) +
+  geom_textbox(data = tribble(~x,~y,~label,
+                              25,300, '<b>Note about the title</b><br>Well, whenever I am at a spot with a great view of the city or landscape, I remember a scene from "Blood In Blood Out." When the characters are looking at the whole city from far away, one of them says, "Check out the view, homies." However, in the Spanish version, they translated that line into "Wachen el paisaje, homies." Nowadays, that scene and line in Spanish have been turned into a meme.'
+                              ),
+               aes(x=x,y=y,label=label),
+               size = 10, color = 'black'
+               ) +
   geom_point(aes(size = radius)) +
   scale_size_area(max_size = 10) +
-  labs(title = "Wachen el paisaje, hommies (Check out the new view, hommies)",
-       subtitle = "Description",
+  labs(title = "Wachen el paisaje, homies (Check out the new view, homies)",
+       subtitle = "Ferris wheels are a well-known attraction in many cities around the globe due to the panoramic view the people can enjoy. The visualization shows some of the most famous Ferris wheels currently operating. <br>The circles' radiuses are size encoded (but not at scale); the maximum height the viewer can be is the distance from the beginning of the line (top) to the center of the circle. Finally, the countries are colour encoded. However, all the information is labelled.",
        caption = "Designed by Isaac Arroyo (@unisaacarroyov on twitter)<br>#TidyTuesday Week 32: Ferris Wheels<br>Data Source: Emil Hvitfeldt (@Emil_Hvitfeldt on twitter) via <span style='font-family:mono'>ferriswheels</span>") +
   theme_void() +
   theme(
@@ -42,5 +47,5 @@ p1 <- datos %>%
     plot.caption = element_textbox_simple(),
   )
 
-p1
+
 
