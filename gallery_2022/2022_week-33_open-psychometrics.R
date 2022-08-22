1e403160a3281d6210160151c07e70278618d819
Isaac Arroyo <isaacarroyov@outlook.com>
2022-08-22 15:06:10 -0500

    [UPDATE] Week 33 -Open Psychometrics - Script - Finished data wrangling :hammer:

--- gallery_2022/2022_week-33_open-psychometrics.R
+++ gallery_2022/2022_week-33_open-psychometrics.R
@@ -1,13 +1,4 @@
 # #TidyTuesday Week 33: Open Psychometrics by Tanya Shapiro
-library(dplyr)
-library(ggplot2)
-library(ggtext)
-library(sysfonts)
-library(showtextdb)
-library(showtext)
-
-# Load data
-tidy_tuesday_data <- tidytuesdayR::tt_load(2022, week = 33)
 
 # Mostrar en una serie de datavis al estilo Cedric Scherer,
 # en la visualizacion de los pinguinos, es decir,
@@ -22,5 +13,66 @@ tidy_tuesday_data <- tidytuesdayR::tt_load(2022, week = 33)
 # Hamilton
 # Stranger Things
 # The Umbrella Academy
+
+library(dplyr)
+library(ggplot2)
+library(ggtext)
+library(sysfonts)
+library(showtext)
+library(factoextra)
+
+# ------ LOAD DATA ------
+psychometrics_characters <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-08-16/psych_stats.csv")
+
+
+
+# ------ SELECT PERSONALITY TRAITS------
+vec_personalities <- c("cocky","independent","genius","eloquent","driven","chatty","adventurous",
+                       "active","decisive","crazy")
+
+# ------ FIND QUESTIONS FOR THE PERSONALITY TRAITS------
+vec_questions <- psychometrics_characters %>%
+  filter(personality %in% vec_personalities) %>%
+  pull(question) %>% unique()
+
+relevant_dataset <- psychometrics_characters %>%
+  filter(question %in% vec_questions) %>%
+  select(char_id, char_name, uni_id, uni_name, question, personality, avg_rating)
+
+# ------ DATA WRANGLING------
+pca_dataset <- relevant_dataset %>%
+  mutate(main_score = case_when(personality %in% vec_personalities ~ avg_rating,
+                                       T ~ 100-avg_rating),
+         main_personality = case_when(personality == 'timid' ~ 'cocky',
+                                      personality == 'codependent' ~ 'independent',
+                                      personality == 'dunce' ~ 'genius',
+                                      personality == 'unpolished' ~ 'eloquent',
+                                      personality == 'unambitious' ~ 'driven',
+                                      personality == 'reserved' ~ 'chatty',
+                                      personality == 'stick-in-the-mud' ~ 'adventurous',
+                                      personality == 'slothful' ~ 'active',
+                                      personality == 'hesitant' ~ 'decisive',
+                                      personality == 'sane' ~ 'crazy',
+                                      T ~ personality
+                                      )) %>%
+  select(char_id, char_name, uni_id, uni_name, main_personality, main_score) %>%
+  tidyr::pivot_wider(names_from = main_personality, values_from = main_score)
+
+
+# ------ PCA------
+numeric_data <- pca_dataset %>% select_if(is.numeric)
+pca_results <- prcomp(numeric_data)
+fviz_eig(pca_results)
+
+# Keep the firts 3 PCA's
+df <- pca_dataset %>%
+  bind_cols(pca_results$x %>%
+  as_tibble() %>%
+  select(PC1,PC2,PC3))
+
+# ------ DATA VISUALIZATION------
+
+
+
 
 
