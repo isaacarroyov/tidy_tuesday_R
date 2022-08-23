72d058a90689e9d6b1e57a328ef623656864abcd
Isaac Arroyo <isaacarroyov@outlook.com>
2022-08-22 20:41:42 -0500

    [UPDATE] Week 33 - Open Psychometrics - Script + Image -> First draft DataVis

--- gallery_2022/2022_week-33_open-psychometrics.R
+++ gallery_2022/2022_week-33_open-psychometrics.R
@@ -25,6 +25,7 @@ library(showtext)
 library(factoextra)
 
 
+
 # ------ LOAD DATA ------
 psychometrics_characters <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-08-16/psych_stats.csv")
 
@@ -75,28 +76,61 @@ df <- pca_dataset %>%
   select(PC1,PC2))
 
 # ------ DATA VISUALIZATION------
-fav_shows <- c('Avatar: The Last Airbender', 'Gossip Girl', 'Hamilton', 'Stranger Things', 'The Umbrella Academy')
+font_add_google("Comfortaa", "titleFont")
+font_add_google("Josefin Sans", "bodyFont")
+showtext_auto()
 
-set.seed(11)
+
+title_font <- "titleFont"
+body_font <- "bodyFont"
+
+
+fav_shows <- c('Avatar: The Last Airbender', 'Gossip Girl', 'Hamilton', 'Stranger Things', 'The Umbrella Academy')
+title_text <- "The Universe of Personalities"
+subtitle_text <- "A fictional character does not have a real personality, but people might perceive it to have one. Thanks to this dataset, we can view the personality spectrum of the characters from more than 50 TV Shows or movies.<br>I selected ten personality traits (cocky, independent, genius, eloquent, driven, chatty, adventurous, active, decisive and crazy). Then, in order to see them in a 2-dimensional plane, I performed a PCA on them.<br>The highlighted points are characters from my favourite TV shows, The Umbrella Academy, Gossip Girl, Avatar: The Last Airbender, Hamilton and The Umbrella Academy."
+caption_text <- "Designed by Isaac Arroyo (@unisaacarroyov on Twitter) <br>#TidyTuesday Week 33: Open Psychometrics by Tanya Shapiro (@tanya_shapiro on Twitter)"
+
+
 p1 <- df %>%
   ggplot(aes(x=PC1, y= PC2, colour= uni_name)) +
   geom_point() +
-  gghighlight(uni_name %in% fav_shows, max_highlight = 100) +
-  geom_label_repel(aes(label=char_name), max.overlaps = 100) +
-  scale_colour_manual(values = met.brewer("VanGogh2", n=5)) +
-  labs(title="Cool title",
-       subtitle = "Description",
-       caption = "Designed by Isaac Arroyo (@unisaacarroyov on Twitter) <br>#TidyTuesday Week 33: Open Psychometrics by Tanya Shapiro (@tanya_shapiro on Twitter)") + 
+  gghighlight(uni_name %in% fav_shows, max_highlight = 50,
+              unhighlighted_params = list(color='gray50', alpha = 0.5)
+              ) +
+  geom_label_repel(aes(label=char_name), max.overlaps = 50, seed = 11,
+                   force = 2, nudge_x = 10,
+                   family = body_font,
+                   ) +
+  scale_colour_manual(values = met.brewer("Java", n=5, direction = -1)) +
+  labs(title= title_text,
+       subtitle = subtitle_text,
+       caption = caption_text) + 
   theme_void() +
   theme(
+    # Background
+    plot.background = element_rect(fill = '#F6E3BD', colour = '#F6E3BD'),
     # Title
     plot.title.position = "plot",
-    plot.title = element_textbox(),
+    plot.title = element_textbox(family = title_font, face = 'bold',
+                                 size = rel(9),
+                                 padding = maring(0,0,0,0),
+                                 margin = margin(0.125,0,0,0, unit = "in")
+                                 ),
     # Subtitle
-    plot.subtitle = element_textbox(),
+    plot.subtitle = element_textbox(family = body_font, face = 'plain', size = rel(3.5),
+                                    lineheight = 0.2,
+                                    width = unit(7.5,'in'),
+                                    padding = margin(0,0,0,0),
+                                    margin = margin(0,0,0,0, unit = "in")
+                                    ),
     # Caption
     plot.caption.position = "plot",
-    plot.caption = element_textbox(halign = 0, hjust = 0,),
+    plot.caption = element_textbox(halign = 0, hjust = 0, family = body_font, face = 'bold',
+                                   size = rel(2),
+                                   lineheight = 0.5,
+                                   padding = margin(0,0,0,0),
+                                   margin = margin(0,0,0,0, unit = "in")
+                                   )
     
   )
     
