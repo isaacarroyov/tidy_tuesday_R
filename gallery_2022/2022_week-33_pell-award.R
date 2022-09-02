35eb6e89f627c2a1b93e2c23eb0aa7fa014b77b8
Isaac Arroyo <isaacarroyov@outlook.com>
2022-09-02 01:06:32 -0500

    [UPDATE] Week 35 - Pell Award - Script -> Define type of visualization (spanish)

--- gallery_2022/2022_week-33_pell-award.R
+++ gallery_2022/2022_week-33_pell-award.R
@@ -10,4 +10,12 @@ library(MetBrewer)
 # LOAD DATA
 pell <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-08-30/pell.csv')
 
-pell
+# En la data-vis intentare complementar el trabajo de @BlakeRobMills
+# La visualizacion constará de kdplots + jitterplots en una serie de tiempo
+# Cada punto sera el AWARD/RECIPIENT para obtener un "promedio" de USD per student
+# Los puntos que seran resaltados serán las Ivy League
+# Con los kde plots se vera la distribucion del dinero por estudiante
+# Con esta visualizacion podremos ver la distribucion de todas las univesidades
+# pero comparando o resaltando las Ivy
+# En esta visualizacion no podre simplificar todas las universidades, esto es algo que se tendra
+# que tomar en cuenta en la visualizacion
