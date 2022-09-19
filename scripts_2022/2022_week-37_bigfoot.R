f8267679d3193a07f754005040f804bb6cb04688
Isaac Arroyo <isaacarroyov@outlook.com>
2022-09-19 13:59:48 -0500

    [UPDATE] Week 37 -  Big foot - Script - Idea
    State the objetive of the data visualization, which is compare variables between Class A & Class B.
    
    Still left to decide the type of charts I'll use.

--- scripts_2022/2022_week-37_bigfoot.R
+++ scripts_2022/2022_week-37_bigfoot.R
@@ -1,4 +1,18 @@
 # ------ #TidyTuesday Week 37: Bigfoot ------
+
+# ------ DATA VISUALIZATION IDEA ------
+# Observando los plots que ha subido la comunidad, en gran parte son mapas de los
+# avistamientos de Big foot en el pais. Algunos de ellos se han comparado con peliculas
+# relacionadas a Big foot o con areas donde habitan osos.
+#
+# Para este caso, quiero explorar las diferencias entre los avistamientos de 
+# Class A and B, ya que es algo completamente nuevo para mi.
+#
+# Se pueden realizar diferentes comparaciones:
+# - palabras que se usan para describir 
+# - condiciones climatologicas 
+
+# ------LOAD LIBRARIES ------
 library(dplyr)
 library(ggplot2)
 library(ggtext)
@@ -6,6 +20,25 @@ library(sysfonts)
 library(showtextdb)
 library(showtext)
 
-# LOAD DATA
+# ------ LOAD DATA ------
 data_bigfoot <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-09-13/bigfoot.csv")
+
+
+
+# ------ DATA WRANGLING ------
+
+
+# ------ DATAVIZ SETTINGS
+# ------ Texts ------
+
+# ------ Colour Palette ------
+
+# ------ Typography ------
+
+# ------ DATA VISUALIZATION ------
+
+# ------ SAVE GGPLOT ------
+
+
+
 
