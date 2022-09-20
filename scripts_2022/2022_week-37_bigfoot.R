1d25176f29fe82e82d7bbd32676157552399a494
Isaac Arroyo <isaacarroyov@outlook.com>
2022-09-20 01:18:10 -0500

    [UPDATE] Week 37 - Big foot - Script - Starting Data wrangling

--- scripts_2022/2022_week-37_bigfoot.R
+++ scripts_2022/2022_week-37_bigfoot.R
@@ -8,9 +8,8 @@
 # Para este caso, quiero explorar las diferencias entre los avistamientos de 
 # Class A and B, ya que es algo completamente nuevo para mi.
 #
-# Se pueden realizar diferentes comparaciones:
-# - palabras que se usan para describir 
-# - condiciones climatologicas 
+# Se pueden realizar diferentes comparaciones pero intentaremos visualizar palabras 
+# que se usan para describir.
 
 # ------LOAD LIBRARIES ------
 library(dplyr)
@@ -26,8 +25,23 @@ data_bigfoot <- readr::read_csv("https://raw.githubusercontent.com/rfordatascien
 
 
 # ------ DATA WRANGLING ------
-
-
+df <- data_bigfoot %>%
+  filter(classification %in% c("Class A","Class B"),
+         !is.na(date)) %>%
+  mutate(date_year = lubridate::year(date)) %>%
+  filter(date_year >= 1950) %>%
+  mutate(date_decade = case_when(date_year %in% seq(1950,1959) ~ "1950s",
+                                 date_year %in% seq(1960,1969) ~ "1960s",
+                                 date_year %in% seq(1970,1979) ~ "1970s",
+                                 date_year %in% seq(1980,1989) ~ "1980s",
+                                 date_year %in% seq(1990,1999) ~ "1990s",
+                                 date_year %in% seq(2000,2009) ~ "2000s",
+                                 date_year %in% seq(2010,2019) ~ "2010s",
+                                 date_year %in% seq(2020,2029) ~ "2020s",
+                                 T ~ NA_character_)) %>%
+  select(classification, observed, date_decade)
+  
+  
 # ------ DATAVIZ SETTINGS
 # ------ Texts ------
 
