fc31e0295a48ce919d19963cc0718461f6a563a6
Isaac Arroyo <isaacarroyov@outlook.com>
2022-10-04 01:13:19 -0500

    [UPDATE] Week 37 - Big Foot - Script - Starting to create network of bigrams

--- scripts_2022/2022_week-37_bigfoot.R
+++ scripts_2022/2022_week-37_bigfoot.R
@@ -5,11 +5,8 @@
 # avistamientos de Big foot en el pais. Algunos de ellos se han comparado con peliculas
 # relacionadas a Big foot o con areas donde habitan osos.
 #
-# Para este caso, quiero explorar las diferencias entre los avistamientos de 
-# Class A and B, ya que es algo completamente nuevo para mi.
-#
-# Se pueden realizar diferentes comparaciones pero intentaremos visualizar palabras 
-# que se usan para describir.
+# Para este caso, quiero explorar los bigramas de las descripciones/testinomios
+# y la major manera es a traves de una red (network)
 
 # ------LOAD LIBRARIES ------
 library(dplyr)
@@ -19,6 +16,15 @@ library(sysfonts)
 library(showtextdb)
 library(showtext)
 
+# ------ TEXT MINING LIBRARIES ------
+# install.packages("tidytext")
+library(tidytext)
+
+
+# ------ NETWORK LIBRARIES ------
+library(ggraph)
+library(tidygraph)
+
 # ------ LOAD DATA ------
 data_bigfoot <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-09-13/bigfoot.csv")
 
@@ -26,31 +32,74 @@ data_bigfoot <- readr::read_csv("https://raw.githubusercontent.com/rfordatascien
 
 # ------ DATA WRANGLING ------
 df <- data_bigfoot %>%
-  filter(classification %in% c("Class A","Class B"),
-         !is.na(date)) %>%
-  mutate(date_year = lubridate::year(date)) %>%
-  filter(date_year >= 1950) %>%
-  mutate(date_decade = case_when(date_year %in% seq(1950,1959) ~ "1950s",
-                                 date_year %in% seq(1960,1969) ~ "1960s",
-                                 date_year %in% seq(1970,1979) ~ "1970s",
-                                 date_year %in% seq(1980,1989) ~ "1980s",
-                                 date_year %in% seq(1990,1999) ~ "1990s",
-                                 date_year %in% seq(2000,2009) ~ "2000s",
-                                 date_year %in% seq(2010,2019) ~ "2010s",
-                                 date_year %in% seq(2020,2029) ~ "2020s",
-                                 T ~ NA_character_)) %>%
-  select(classification, observed, date_decade)
+  filter(classification %in% c("Class A","Class B")) %>%
+  select(classification, observed)
   
+# ------ TIDYING THE TEXT ------
+# Obtener bigramas
+df_bigrams_class_decade <- df %>%
+  unnest_tokens(output = bigram,
+                input = observed,
+                token = "ngrams",
+                n = 2)
   
-# ------ DATAVIZ SETTINGS
-# ------ Texts ------
+# Quitar bigramas sin stop-words
+df_bigrams_class_decade_clean <- df_bigrams_class_decade %>%
+  tidyr::separate(col = bigram,
+                  into = c("word_1", "word_2"),
+                  sep = " ") %>%
+  filter(!word_1 %in% stop_words$word,
+         !word_2 %in% stop_words$word)
 
+
+# Filter by adjectives (at least one)
+vec_idx_adjectives <- which(parts_of_speech$pos == "Adjective")
+vec_words_adjectives <- parts_of_speech$word[vec_idx_adjectives]
+
+df_bigrams_class_decade_clean_adj <- df_bigrams_class_decade_clean %>%
+  filter(word_1 %in% vec_words_adjectives | word_2 %in% vec_words_adjectives)
+
+# Create network
+graph_bigrams <- df_bigrams_class_decade_clean_adj %>%
+  count(word_1, word_2, sort = T) %>%
+  filter(n >= 15) %>%
+  igraph::graph_from_data_frame() %>%
+  as_tbl_graph() %>%
+  activate(nodes) %>%
+  mutate(degree_score = centrality_degree(),
+         eigen_score = centrality_eigen())
+
+
+graph_bigrams %>%
+  activate(nodes) %>%
+  as_tibble() %>%
+  arrange(-eigen_score) %>%
+  print(n=10)
+
+# ------ DATAVIZ SETTINGS ------
+# ------ Texts ------
+title_text <- "Describing Big Foot"
+subtitle_text <- "Bigrams used in class A and class B sights"
+subtitle_text_highlight_bigrams <- "Another text"
+caption_text <- "Designed by Isaac Arroyo (@unisaacarroyov on twitter)<br>#TidyTuesday Week 31: Big Foot"
 # ------ Colour Palette ------
 
 # ------ Typography ------
 
 # ------ DATA VISUALIZATION ------
-
+set.seed(11)
+graph_bigrams %>%
+  ggraph(layout = 'fr') +
+  geom_edge_arc(edge_width = 0.3,
+                strength = 0.2,
+                end_cap = circle(3,'pt')
+                ) + 
+  geom_node_point(aes(size = eigen_score)) +
+  labs(title = title_text, subtitle = subtitle_text, caption = caption_text) +
+  theme_void() +
+  theme(
+    legend.position = "none"
+  )
 # ------ SAVE GGPLOT ------
 
 
