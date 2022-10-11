c410618029a946f964464bf28e761864a8999309
Isaac Arroyo <isaacarroyov@outlook.com>
2022-10-11 10:45:11 -0500

    [UPDATE] Week 40 - Product Hunt - Script - Data wrangling :white_check_mark:

--- scripts_2022/2022_week-40_product-hunt.R
+++ scripts_2022/2022_week-40_product-hunt.R
@@ -10,6 +10,25 @@ product_hunt <- readr::read_csv('https://raw.githubusercontent.com/rfordatascien
 glimpse(product_hunt)
 
 # ------ DATA VIZ IDEA ------
-# Visualizar las palabras mas usadas para describir productos de Apple
+# Visualizar las palabras mas usadas para describir productos de
+# 
+
+# ------ DATA WRANGLING ------
+# Forma de la tabla
+# category_tag | decripcion del producto con ese tag (no importa que se repita abajo) | Fecha en la que se publico el producto
+df <- product_hunt %>%
+  select(category_tags, product_description, release_date) %>%
+  drop_na(product_description) %>%
+  mutate(category_tags = str_remove_all(category_tags, pattern = "[''\\[\\]]"),
+         category_tags = str_split(string = category_tags, pattern = ",")) %>%
+  unnest_longer(category_tags) %>%
+  mutate(category_tags = str_squish(category_tags),
+         release_date = ymd(release_date))
+
+
+  
+
+
+
 
 
