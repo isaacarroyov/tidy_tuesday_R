c1d09cf996ff9d1cde925fd985073f54ffd6cf21
Isaac Arroyo <isaacarroyov@outlook.com>
2022-10-11 12:25:24 -0500

    [UPDATE] Week 40 - Product Hunt - Script - Finished data wrangling 4viz

--- scripts_2022/2022_week-40_product-hunt.R
+++ scripts_2022/2022_week-40_product-hunt.R
@@ -24,7 +24,24 @@ df <- product_hunt %>%
          category_tags = str_split(string = category_tags, pattern = ","),
          release_date = ymd(release_date))
 
-  
+# ------ DATA WRANGLING (data viz 1) ------
+df_dataviz_1 <- df %>%
+  mutate(week_of_year = week(release_date),
+         year_date = year(release_date)) %>%
+  count(year_date, week_of_year) %>%
+  filter(week_of_year != 53) %>%
+  rename(number_of_releases = n)
 
-
-
+# ------ DATA WRANGLING (data viz 2) ------
+# Focusing on 2017 weeks 33, 34 and 35
+df_dataviz_2 <- df %>%
+  mutate(date_year = year(release_date),
+         week_of_year = week(release_date)) %>%
+  filter(date_year == 2017,
+         week_of_year %in% 33:35) %>%
+  unnest_longer(category_tags) %>%
+  mutate(category_tags = str_squish(category_tags),
+         category_tags = str_to_title(category_tags),
+         category_tags = if_else(category_tags == "Iphone", "iPhone", category_tags)) %>%
+  count(category_tags, sort = T) %>%
+  rename(category_tags_times_used = n)
