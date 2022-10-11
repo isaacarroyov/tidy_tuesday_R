36e9e0906b18fea0b8c9ba8b1b9d80ea653599f5
Isaac Arroyo <isaacarroyov@outlook.com>
2022-10-10 20:02:19 -0500

    [UPDATE] Week 39 - Artists in USA - Script - Finished Data wrangling 4 viz

--- scripts_2022/2022_week-39_artists-in-usa.R
+++ scripts_2022/2022_week-39_artists-in-usa.R
@@ -4,9 +4,8 @@
 # ------ LOAD PACKAGES ------
 library(tidyverse)
 library(ggtext)
-# library(ggsankey)
-# install.packages("ggalluvial")
-library(ggalluvial)
+remotes::install_github("AllanCameron/geomtextpath")
+library(geomtextpath)
 library(sysfonts)
 library(showtextdb)
 library(showtext)
@@ -19,16 +18,12 @@ artists <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/ti
 glimpse(artists)
 
 # ------ DATA WRANGLING ------
-# Note: a concentration_labor_force >= 1 is that the state's labor force in an occupation
-#       is +100 percent greater than the occupation's national labor force share
-df_hispanic_artists <- artists %>%
-  filter(race == 'Hispanic') %>%
-  drop_na(artists_share, location_quotient) %>%
+df <- artists %>%
+  filter(race == "Hispanic") %>%
+  replace_na(list(artists_share = 0, location_quotient = 0)) %>%
   mutate(artists_share_perc = artists_share * 100,
-         concentration_labor_force = (location_quotient - 1)*100) %>%
-  select(state, type, artists_share_perc, concentration_labor_force) %>%
-  # Group into 6-7 big categories: "Art expression"
-  mutate(art_expression = case_when(type %in% c("Designers", "Fine Artists, Art Directors, And Animators", "Photographers") ~ "Visual",
+         concentration_labor_force = (location_quotient - 1)*100,
+         art_expression = case_when(type %in% c("Designers", "Fine Artists, Art Directors, And Animators", "Photographers") ~ "Visual",
                                     type %in% c("Architects","Landscape Architects") ~ "Architecture",
                                     type %in% c("Writers And Authors") ~ "Literature",
                                     type %in% c("Musicians", "Music Directors And Composers") ~ "Music",
@@ -41,39 +36,78 @@ df_hispanic_artists <- artists %>%
                                            art_expression == "Music" ~ "Music (Musicians, Music Directors and Composers)",
                                            art_expression == "Performance" ~ "Performance (Actors, Announcers, Entertainers, Producers and Directors)",
                                            art_expression == "Visual" ~ "Visual (Designers, Photographers, Fine Artists, Art Directors and Animators)",
-                                           T ~ "Another art expression"),
-         # Discretize % of artists in the workforce : "Percentage of all workers"
-         artists_share_perc_bins = case_when(artists_share_perc < 0.2 ~ 1,
-                                             artists_share_perc >= 0.2 & artists_share_perc <= 0.4 ~ 2,
-                                             artists_share_perc > 0.4 & artists_share_perc <= 0.6 ~ 3,
-                                             artists_share_perc > 0.6 & artists_share_perc <= 0.8 ~ 4,
-                                             T ~ 5),
-         artists_share_perc_bins = ordered(artists_share_perc_bins,
-                                           levels = c(1,2,3,4,5),
-                                           labels = c("Less than 0.2% of all workers in the state",
-                                                      "Between 0.2% - 0.4% of all workers in the state",
-                                                      "Between 0.4% - 0.6% of all workers in the state",
-                                                      "Between 0.6% - 0.8% of all workers in the state",
-                                                      "Between 0.8% - 1% of all workers in the state")),
-         # Discretize concentration_labor_force: "Percentage of occupation's national labor force share. Negative values mean less than the national labor force share and positive the opposite"
-         concentration_labor_force_bins = case_when(concentration_labor_force <= -50 ~ 1,
-                                                    concentration_labor_force > -50 & concentration_labor_force <= 0 ~ 2,
-                                                    concentration_labor_force > 0 & concentration_labor_force <= 50 ~ 3,
-                                                    concentration_labor_force > 50 & concentration_labor_force <= 100 ~ 4,
-                                                    T ~ 5),
-         concentration_labor_force_bins = ordered(concentration_labor_force_bins,
-                                                  levels = c(1,2,3,4,5),
-                                                  labels = c("Between 50% and 100% <b>less</b> than the national labor force share",
-                                                             "Between 0% and 50% <b>less</b> than the national labor force share",
-                                                             "Between 0% and 50% <b>more</b> than the national labor force share",
-                                                             "Between 50% and 100% <b>more</b> than the national labor force share",
-                                                             "<b>More</b> than 100% of the national labor force share")))
+                                           T ~ "Another art expression")) %>%
+  rename(state_usa = state) %>%
+  group_by(state_usa, art_expression) %>%
+  summarise(avg_artists_share_perc = mean(artists_share_perc),
+            avg_concentration_labor_force = mean(concentration_labor_force)) %>%
+  ungroup()
+  
+# ------ DATA VISUALIZATION ------
+# Text
+# Typography
 
-# TODO: try a variation of barcharts
+
 
 
 
 # ------ IGNORE IT (tried to make an alluvial/sankey)
+# library(ggsankey)
+# install.packages("ggalluvial")
+# library(ggalluvial)
+# 
+# ------ DATA WRANGLING (focused on alluvial/sankey)------
+# Note: a concentration_labor_force >= 1 is that the state's labor force in an occupation
+#       is +100 percent greater than the occupation's national labor force share
+# df_hispanic_artists <- artists %>%
+#   filter(race == 'Hispanic') %>%
+#   drop_na(artists_share, location_quotient) %>%
+#   mutate(artists_share_perc = artists_share * 100,
+#          concentration_labor_force = (location_quotient - 1)*100) %>%
+#   select(state, type, artists_share_perc, concentration_labor_force) %>%
+#   # Group into 6-7 big categories: "Art expression"
+#   mutate(art_expression = case_when(type %in% c("Designers", "Fine Artists, Art Directors, And Animators", "Photographers") ~ "Visual",
+#                                     type %in% c("Architects","Landscape Architects") ~ "Architecture",
+#                                     type %in% c("Writers And Authors") ~ "Literature",
+#                                     type %in% c("Musicians", "Music Directors And Composers") ~ "Music",
+#                                     type %in% c("Dancers And Choreographers") ~ "Dance",
+#                                     type %in% c("Producers And Directors", "Entertainers","Actors", "Announcers") ~ "Performance",
+#                                     T ~ "Another art expression"),
+#          art_expression = case_when(art_expression == "Architecture" ~ "Architecture (Architects and Landscape Architects)",
+#                                            art_expression == "Dance" ~ "Dance (Dancers and Choreographers)",
+#                                            art_expression == "Literature" ~ "Literature (Writers and Authors)",
+#                                            art_expression == "Music" ~ "Music (Musicians, Music Directors and Composers)",
+#                                            art_expression == "Performance" ~ "Performance (Actors, Announcers, Entertainers, Producers and Directors)",
+#                                            art_expression == "Visual" ~ "Visual (Designers, Photographers, Fine Artists, Art Directors and Animators)",
+#                                            T ~ "Another art expression"),
+#          # Discretize % of artists in the workforce : "Percentage of all workers"
+#          artists_share_perc_bins = case_when(artists_share_perc < 0.2 ~ 1,
+#                                              artists_share_perc >= 0.2 & artists_share_perc <= 0.4 ~ 2,
+#                                              artists_share_perc > 0.4 & artists_share_perc <= 0.6 ~ 3,
+#                                              artists_share_perc > 0.6 & artists_share_perc <= 0.8 ~ 4,
+#                                              T ~ 5),
+#          artists_share_perc_bins = ordered(artists_share_perc_bins,
+#                                            levels = c(1,2,3,4,5),
+#                                            labels = c("Less than 0.2% of all workers in the state",
+#                                                       "Between 0.2% - 0.4% of all workers in the state",
+#                                                       "Between 0.4% - 0.6% of all workers in the state",
+#                                                       "Between 0.6% - 0.8% of all workers in the state",
+#                                                       "Between 0.8% - 1% of all workers in the state")),
+#          # Discretize concentration_labor_force: "Percentage of occupation's national labor force share. Negative values mean less than the national labor force share and positive the opposite"
+#          concentration_labor_force_bins = case_when(concentration_labor_force <= -50 ~ 1,
+#                                                     concentration_labor_force > -50 & concentration_labor_force <= 0 ~ 2,
+#                                                     concentration_labor_force > 0 & concentration_labor_force <= 50 ~ 3,
+#                                                     concentration_labor_force > 50 & concentration_labor_force <= 100 ~ 4,
+#                                                     T ~ 5),
+#          concentration_labor_force_bins = ordered(concentration_labor_force_bins,
+#                                                   levels = c(1,2,3,4,5),
+#                                                   labels = c("Between 50% and 100% <b>less</b> than the national labor force share",
+#                                                              "Between 0% and 50% <b>less</b> than the national labor force share",
+#                                                              "Between 0% and 50% <b>more</b> than the national labor force share",
+#                                                              "Between 50% and 100% <b>more</b> than the national labor force share",
+#                                                              "<b>More</b> than 100% of the national labor force share")))
+# 
+# 
 # ------ DATA VISUALIZATION (ggalluvial) ------
 # ------ specific changes for dataviz ------
 # df_hispanic_artists_alluvial_all <- df_hispanic_artists %>%
