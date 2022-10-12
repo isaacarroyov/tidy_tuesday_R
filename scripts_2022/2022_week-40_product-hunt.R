101847dd29456aa7dc8a1c941a252af628210dcd
Isaac Arroyo <isaacarroyov@outlook.com>
2022-10-11 22:09:15 -0500

    [UPDATE] Week 40 - Product Hunt - Script -> Finished data viz 1 (lines)

--- scripts_2022/2022_week-40_product-hunt.R
+++ scripts_2022/2022_week-40_product-hunt.R
@@ -2,6 +2,8 @@
 library(tidyverse)
 library(lubridate)
 library(ggtext)
+library(MetBrewer)
+library(geomtextpath)
 library(sysfonts)
 library(showtext)
 
@@ -32,6 +34,10 @@ df_dataviz_1 <- df %>%
   filter(week_of_year != 53) %>%
   rename(number_of_releases = n)
 
+df %>%
+  filter(week(release_date) %in% 33:35,
+         year(release_date) == 2017)
+
 # ------ DATA WRANGLING (data viz 2) ------
 # Focusing on 2017 weeks 33, 34 and 35
 df_dataviz_2 <- df %>%
@@ -51,38 +57,86 @@ df_dataviz_2 <- df %>%
 # ------ DATA VIZ ------  
 
 # ------ typography ------  
+font_add_google("Merriweather", "title_font")
+font_add_google("Mulish", "body_font")
+showtext_auto()
+
+title_font <- "title_font"
+body_font <- "body_font"
+
 
 # ------ text ------  
-title_text_1 <- "Title 1"
-subtitle_text_1 <- "Subtitle 1"
-title_text_2 <- "Title 2"
-subtitle_text_2 <- "Subtitle 2"
+title_text_1 <- "Changes in Product Hunt: 3 weeks in 2017"
+subtitle_text_1 <- "Product Hunt is the internet's largest social network and clearinghouse for apps. It's mainly used to promote tech products.<br>Like every new idea, at first, the inflow of products and releases is low, but eventually, it increases. So when did it happen to Product Hunt? During August and September 2017 (between the 33rd and 35th week of the year), the site registered a significant increase in product releases."
+subtitle_text_2 <- "What types of releases happened during those three weeks (omitting Tech)? Mostly **Productivity**, **Web Apps** and **iPhone**."
 caption_text <- "Designed by Isaac Arroyo (@unisaacarroyov on twitter)<br>#TidyTuesday Week 40: Product Hunt<br>Dataset provided by components.one"
 
-# ------ theme ------  
+# ------ theme ------ 
+theme_set(theme_minimal())
+theme_update(
+  legend.position = "none",
+  # Title
+  plot.title.position = "plot",
+  plot.title = element_textbox(family = title_font,
+                               face = "bold",
+                               lineheight = 0.2,
+                               size = rel(8),
+                               width = unit(625, "pt"),
+                               hjust = 0.5),
+  # Subtitle
+  plot.subtitle = element_textbox(family = body_font,
+                                  lineheight = 0.3,
+                                  size = rel(4),
+                                  width = unit(625, "pt"),
+                                  hjust = 0.5,
+                                  halign = 0),
+  # Caption
+  plot.caption.position = "plot",
+  plot.caption = element_textbox(family = body_font,
+                                 lineheight = 0.3,
+                                 size = rel(3),
+                                 width = unit(625,"pt"),
+                                 hjust = 0.5,
+                                 halign = 0),
+)
 
 # ------ line chart ------  
-df_dataviz_1 %>%
+p1 <- df_dataviz_1 %>%
   mutate(year_date = factor(year_date)) %>%
   ggplot(aes(x = week_of_year, y = number_of_releases, colour = year_date)) +
+  geom_textsegment(label = "50 releases", x = 0, xend = 52, y = 50, yend = 50, family = body_font, size = 4.5, spacing = -100, hjust = 0, vjust = -0.1, linewidth = 0.2, colour = 'gray60', linecolour = 'gray50', linetype = 'dashed') +
+  geom_textsegment(label = "150 releases", x = 0, xend = 52, y =150, yend = 150, family = body_font, size = 4.5, spacing = -100, hjust = 0, vjust = -0.1, linewidth = 0.2, colour = 'gray60', linecolour = 'gray50', linetype = 'dashed') +
+  geom_textsegment(label = "250 releases", x = 0, xend = 52, y = 250, yend = 250, family = body_font, size = 4.5, spacing = -100, hjust = 0, vjust = -0.1, linewidth = 0.2, colour = 'gray60', linecolour = 'gray50', linetype = 'dashed') +
   geom_line() +
-  gghighlight::gghighlight(number_of_releases > 0, use_direct_label = F) +
-  facet_wrap(~year_date, ncol = 2) +
-  labs(title = title_text_1, subtitle = subtitle_text_1, caption = caption_text) +
+  gghighlight::gghighlight(number_of_releases > 0,
+                           use_direct_label = F,
+                           unhighlighted_params = list(colour = 'gray80', size = 0.5, alpha = 0.3)) +
+  scale_x_continuous(breaks = seq(10,40,10), labels = paste("week\n",seq(10,40,10))) +
+  scale_colour_manual(values = met.brewer(name = "Tam", n = 8)) +
+  facet_wrap(~year_date, ncol = 2, scales = "free") +
+  labs(title = title_text_1, subtitle = subtitle_text_1) +
   theme(
-    legend.position = "none",
-    # Plot
-    # Title
-    plot.title.position = "plot",
-    plot.title = element_text(),
-    # Subtitle
-    plot.subtitle = element_textbox(),
-    # Caption
-    plot.caption.position = "plot",
-    plot.caption = element_textbox(),
-    # Axis
+    # Grid*
+    panel.grid = element_blank(),
+    # Axis*
     axis.title = element_blank(),
-    # Facets
+    axis.text.y = element_blank(),
+    axis.line.x = element_line(colour = 'gray60', size = 0.4),
+    axis.ticks.x = element_line(colour = 'gray60',
+                                size = 0.6,
+                                arrow = arrow(length = unit(1,"pt"),
+                                              ends = "last",
+                                              angle = 30,
+                                              type = "closed")),
+    axis.text.x = element_text(family = body_font,
+                             lineheight = 0.3,
+                             size = rel(2.5)),
+    # Facets*
+    strip.text = element_text(family = title_font,
+                              face = "bold",
+                              lineheight = 0.2,
+                              size = rel(3))
   )
 
 # ------ bar chart ------
+
