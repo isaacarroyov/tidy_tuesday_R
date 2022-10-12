099c18507fc10043a7a7150434af1419de9c0f7c
Isaac Arroyo <isaacarroyov@outlook.com>
2022-10-12 01:37:42 -0500

    [UPDATE] Week 41 - Yarn - Script -> Finished dataviz idea :bulb:

--- scripts_2022/2022_week-41_ravelry-yarn.R
+++ scripts_2022/2022_week-41_ravelry-yarn.R
@@ -8,4 +8,12 @@ library(showtext)
 # ------ LOAD DATA ------
 yarn <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-10-11/yarn.csv')
 
-yarn
+# ------ DATAVIZ IDEA ------
+# No tengo ni la mas minima idea sobre el tema de yarn, sin embargo es una perfecta 
+# oportunidad para hacer una visualizacion poco convencional nada mas por el gusto
+# de crear algo.
+# Exploraremos diferentes visualizaciones para diferentes columnas
+
+# ------ DATA WRANGLING ------
+df <- yarn %>%
+  select(yarn_company_name, rating_average, rating_count, grams, yardage, yarn_weight_wpi, yarn_weight_name)
